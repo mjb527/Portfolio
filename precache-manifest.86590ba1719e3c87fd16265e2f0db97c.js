f8512743a46d6e9be172ca81ccddc37bbc860694
Matt <mbatko0914@gmail.com>
2020-07-27 05:23:40 -0400

    Updates

---
+++ precache-manifest.86590ba1719e3c87fd16265e2f0db97c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fe1837297626898815eb",
+    "url": "/Portfolio/static/css/main.78e2ad08.chunk.css"
+  },
+  {
+    "revision": "fe1837297626898815eb",
+    "url": "/Portfolio/static/js/main.6d7588ec.chunk.js"
+  },
+  {
+    "revision": "6b64d540f1308ee928ad",
+    "url": "/Portfolio/static/js/runtime~main.132fbc8c.js"
+  },
+  {
+    "revision": "092325f8fa5bf1c96dbc",
+    "url": "/Portfolio/static/js/2.dc02ade5.chunk.js"
+  },
+  {
+    "revision": "f6e3fd753826eb50bbc030949436905d",
+    "url": "/Portfolio/index.html"
+  }
+];
